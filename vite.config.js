2745779ac8de13918e542590094fbb5f2ae399e7
wangkc <wkclz@qq.com>
2022-09-04 17:22:17 +0800

    dev 连接本地

--- vite.config.js
+++ vite.config.js
@@ -28,16 +28,14 @@ export default defineConfig(({ mode, command }) => {
       port: 80,
       host: true,
       open: true,
-      /*
       proxy: {
         // https://cn.vitejs.dev/config/#server-proxy
         '/dev-api': {
-          target: 'http://localhost:8080',
+          target: 'http://localhost:10997',
           changeOrigin: true,
           rewrite: (p) => p.replace(/^\/dev-api/, '')
         }
       }
-      */
     },
     //fix:error:stdin>:7356:1: warning: "@charset" must be the first rule in the file
     css: {
